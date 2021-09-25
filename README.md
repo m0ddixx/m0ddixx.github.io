b4560dd40162a321533e94ccfb270c06a5ec45c7
m0ddixx <m0ddixx@users.noreply.github.com>
2021-09-25 02:32:29 +0000

    deploy: de00c150665b4741bdc511d70eb0f24ca925324d

---
+++ README.md
@@ -0,0 +1,1 @@
+# m0ddixx.github.io
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  purge: [`**/*.html`],
+  darkMode: "media",
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme("colors.gray.100"),
+
+            a: {
+              color: theme("colors.blue.100"),
+              "&:hover": {
+                color: theme("colors.blue.100"),
+              },
+            },
+            h1: {
+              color: theme("colors.gray.400"),
+            },
+            h2: {
+              color: theme("colors.gray.400"),
+            },
+            h3: {
+              color: theme("colors.gray.400"),
+            },
+            h4: {
+              color: theme("colors.gray.400"),
+            },
+            h5: {
+              color: theme("colors.gray.400"),
+            },
+            h6: {
+              color: theme("colors.gray.400"),
+            },
+
+            strong: {
+              color: theme("colors.gray.300"),
+            },
+
+            code: {
+              color: theme("colors.gray.300"),
+            },
+            p: {
+              color: theme("colors.gray.200"),
+            },
+
+            figcaption: {
+              color: theme("colors.gray.500"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
